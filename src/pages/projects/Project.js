819e45e59de60745a8eeb1a3c1bb94acacad3945
Abel Lu <aluprof4@gmail.com>
2023-09-17 02:12:35 -0400

    projects page

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Project = (props) => {
     const { project } = props;
     return (
-        <Link className="project" style={{textDecoration: "none"}} to={project.url}>
+        <Link className="project" style={{textDecoration: "none"}} to={project.url} target="_blank" rel="noopener noreferrer">
             <img style={{borderRadius: project.borderRadius}} src={require('../../images/' + project.imagePath)} alt={project.alt} />
             <b>{project.title}</b>
             <p>{project.description}</p>
